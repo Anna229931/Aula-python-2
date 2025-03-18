195097656e9edd43a25a6b9184d28a9992a5bc97
Anna229931 <annafran2019@gmail.com>
2025-03-17 22:49:17 -0300

    Minha primeira aula em python :)

--- primeiro programa.py
+++ primeiro programa.py
@@ -1,1 +1,28 @@
-print ("Hello World")
+#Exercício 1
+print ("Anna\n")
+#print()
+#Exercício 2
+a=3
+b=5
+print(2*a*3*b)
+print()
+#Exercício 3
+# c=2
+# d=4
+# e=6
+# soma = c+d+e
+# print(soma)
+print()
+# num1 = int(input("Número 1 = "))
+# num2 = int(input("Número 2 = "))
+# num3 = int(input("Número 3 = "))
+# soma = num1+num2+num3
+# print(soma)
+print()
+#int = inteiro, float = decimal
+#para diminuir o número de casas decimais: print("o número com duas casas ecimais é{número:.2f}.")
+num1 = float(input("Número 1 = "))
+num2 = float(input("Número 2 = "))
+num3 = float(input("Número 3 = "))
+soma = num1+num2+num3
+print(f"soma = {soma:.2f}!")
