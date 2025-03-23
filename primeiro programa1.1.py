125b16b47f3dd144a0bc2240c35ba3cb90e235d4
Anna229931 <annafran2019@gmail.com>
2025-03-23 18:18:36 -0300

    Programa if else

--- primeiro programa1.1.py
+++ primeiro programa1.1.py
@@ -43,7 +43,34 @@ print()
 # valor_milimetros= met*1000
 # print(f"O valor em milímeros é:{valor_milimetros:.1f}")
 print()
-#Exercício 6
+#Exercício 7
+# valor_salario = float(input("valor do salário atual = "))
+# porcentagem_aumento = float(input("porcentagem do aumento = "))
+# mul = valor_salario * porcentagem_aumento
+# print("O valor final do salário é =", mul+valor_salario)
+print()
+#Exercício 8
+# desconto = float(input("percentual de desconto = "))
+# preco = float(input("preço inicial = "))
+# mul = preco*desconto
+# print("O valor do desconto é =", mul)
+# print("o valor final é = ",preco-mul)
+
+#exercício 9
+# distancia=float(input("Distancia percorrida em metros = "))
+# velocidade = float(input("Velocidade média da viagem em m/s= "))
+# tempo = distancia / velocidade
+# print(f"o tempo da viagem em segundos é ={tempo:.2f}")
+# min = tempo/60
+# print(f"o tempo da viagem em minutos é ={min:.2f}")
+# horas = min / 60
+# print(f"o tempo da viagem em horas = {horas:.2f}")
+
+#Exercício 10
+# graus=float(input("Graus = "))
+# Fi=((((graus*9)/5)+32))
+# print(f"o valor em F é ={Fi:.2f}")
+
 
 
 
---
+++ programa if 1.0.py
@@ -0,0 +1,25 @@
+# a = int(input("Primeiro valor: "))
+# b = int(input("Segundo valor: "))
+# if a > b:
+#     print("O primeiro valor é maior!")
+# else:
+#     print("o segundo é maior!")
+print()
+#exercício 1
+# velocidade = float(input("Velocidade inicial em km/h = "))
+# limite_de_velocidade = 80
+# if velocidade > limite_de_velocidade:
+#     excesso = velocidade - limite_de_velocidade
+#     multa = 5*excesso
+#     print("a multa é de = ", multa)
+# else:
+#     print("sem multa")
+print()
+#exercício 2
+a = int(input("A = "))
+b = int(input("B = "))
+c = int(input("C = "))
+maior = max(a, b, c)
+menor = min(a, b, c)
+print("o maior númeo é =", maior)
+print("o menor número é", menor)
