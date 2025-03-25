b7329e791abca9a870d713701028f14ffddb7fe8
Anna229931 <annafran2019@gmail.com>
2025-03-24 22:38:14 -0300

    24/03

---
+++ aula24_03.py
@@ -0,0 +1,23 @@
+nome = "Anna"
+print(type(nome))
+print()
+d = True
+print(type(d))
+print()
+print(10**3)
+print(10//3)
+print(10%3)
+print()
+nota = 8
+media = 7
+aprovado = nota == media
+print(aprovado)
+print()
+nome_completo = input("nome completo:")
+idade = int(input("idade:"))
+altura = float(input("altura:"))
+print(nome_completo)
+print(idade)
+print(altura)
+
+
