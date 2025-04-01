a709e31f3746b50a3bcb0d5c8b1593c96948b73d
Anna229931 <annafran2019@gmail.com>
2025-03-31 22:49:36 -0300

    qqq

--- programa if 1.0.py
+++ programa if 1.0.py
@@ -16,10 +16,47 @@ print()
 #     print("sem multa")
 print()
 #exercício 2
-a = int(input("A = "))
-b = int(input("B = "))
-c = int(input("C = "))
-maior = max(a, b, c)
-menor = min(a, b, c)
-print("o maior númeo é =", maior)
-print("o menor número é", menor)
+# a = int(input("A = "))
+# b = int(input("B = "))
+# c = int(input("C = "))
+# maior = max(a, b, c)
+# menor = min(a, b, c)
+# print("o maior númeo é =", maior)
+# print("o menor número é", menor)
+# #Exercício 4
+# distancia = float(input("distancia que deseja percorrer"))
+# if distancia <= 200:
+#     preco1 = 0.50 * distancia
+#     print("o preco é R$", preco1)
+# else:
+#     preco2 = 0.45 * distancia
+#     print("o preco é R$",preco2)
+#Exercício 5
+# num1 = float(input("número 1:"))
+# num2 = float(input("número 2:"))
+# operação = int(input("digite a operação que quer realizar:\n 1 - subtração, 2 - adição, 3 - multiplicação, 4 - divisão"))
+# if operação == 1:
+#     sub = num1 - num2
+#     print("%2f"%sub)
+# elif operação == 2:
+#     soma = num1 + num2
+#     print("%2f"%soma)
+# elif operação == 3:
+#     mul = num1 * num2
+#     print("%2f"%mul)
+# elif operação == 4:
+#     div = num1/num2
+#     print("%.2f"%div)
+# Exercicio 6
+valor_casa = float(input("valor da casa:"))
+salario = float(input("seu salário:"))
+anos = int(input("quantos anos irá pagar:"))
+prestação = valor_casa/(anos * 12)
+if anos <= salario*0.3:
+    print("A prestação é:",prestação)
+else:
+    print("prestação negada")
+
+
+
+
