0e4c7b2448fa84b0e7c3b8f7afd35ea0dbe97928
Anna229931 <annafran2019@gmail.com>
2025-05-12 22:21:10 -0300

    aula 4

---
+++ aula12_05.py
@@ -0,0 +1,39 @@
+# n = 1 #contador
+# soma = 0 #acumulador
+# while n <= 10:
+#     x = int(input(f"Digite o número {n}: "))
+#     soma += + x
+#     n += + 1
+#     print(f"Soma = {soma}")
+
+# n = 1 #contador
+# soma = 0 #acumulador
+# while n <= 5:
+#     x = float(input(f"Digite o número {n}: "))
+#     soma += + x
+#     n += + 1
+# print(f"Média = {(soma / n - 1):.2f}")
+
+# soma = 0
+# while True:
+#     num = int(input("Digite um número a somar ou 0 para sair: "))
+#     if num == 0:
+#         break
+#     soma += num
+# print(f"soma = {soma}")
+
+# 10. Escreva um programa que  leia números inteiros do teclado.
+# O programa deve ler os números até que o usuário digite 0 (zero).
+# No final da execução, exiba a quantidade de números digitados,
+# assim como a soma e a média aritmética.
+
+# qnt = 0
+# soma = 0
+# while True:
+#     num = int(input("digite um número ou 0 para sair: "))
+#     if num == 0:
+#         break
+#     qnt += 1
+#     soma += num
+# print(f"a quantidade de numeros digitados é = {qnt}")
+# print(f"a soma é {soma} e a média aritmética é {(soma / qnt):.2f}")
