96bac5158789ec63984c0f35b77691c249faddef
Anna22931 <annafran2019@gmail.com>
2025-09-22 22:40:09 -0300

    sssssssssso

---
+++ string.py
@@ -0,0 +1,18 @@
+# frase = "Hello World"
+# print(len(frase))
+# print(frase[0])
+# print(frase[-1])
+#
+# string1 = "Hello"
+# string2 = "World"
+#  print(string1+ " " +string2)
+# print(string1,string2)
+# string3 = "!"
+# print(string1, string2, string3 *3)
+
+# frase = "Hello World"
+# print(frase[0:6])
+# print(frase[6:])
+
+frase = "Hello World"
+print(type(frase))
